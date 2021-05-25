6c17d77c6103ab0f4504c537c95034638faa5820
trevordanahy <danat19@protonmail.com>
2021-05-25 17:21:45 -0500

    Added layout for the Registration Form.

--- frontend/src/components/control/Auth/RegistrationForm.jsx
+++ frontend/src/components/control/Auth/RegistrationForm.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { AuthLabel, AuthInput, ErrorMsg, AuthBttn } from '../../style/AuthStyles/AuthFormStyles'
 
 export default function RegistrationForm() {
+  const [error, setError] = useState('')
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>Registration</h1>
+      <AuthLabel for="email">Email</AuthLabel>
+      <AuthInput id="email"/>
+      <AuthLabel for="username">Username</AuthLabel>
+      <AuthInput id="username"/>
+      <AuthLabel for="password">Password</AuthLabel>
+      <AuthInput id="password"/>
+      <AuthLabel for="confirmPassword">Confirm Password</AuthLabel>
+      <AuthInput id="confirmPassword"/> 
+      <ErrorMsg>{error}</ErrorMsg> 
+      <AuthBttn>Register</AuthBttn>  
+    </>
   )
 }
