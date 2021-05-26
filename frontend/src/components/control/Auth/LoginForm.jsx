e782393ffb4c564898f456534b6a355e2c19a91b
trevordanahy <danat19@protonmail.com>
2021-05-26 13:19:31 -0500

    Added sumit handler.  Pulled auth state from AuthContext. Fully working.

--- frontend/src/components/control/Auth/LoginForm.jsx
+++ frontend/src/components/control/Auth/LoginForm.jsx
@@ -1,14 +1,32 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
 import {login } from '../../../adapters'
+import { AUTH_LOGIN } from '../../../context/auth-actions'
+import AuthContext from '../../../context/auth-context'
 import { AuthBttn, AuthInput, 
   AuthLabel, ErrorMsg } from '../../style/AuthStyles/AuthFormStyles'
 
 export default function LoginForm() {
+  const {state,dispatch} = useContext(AuthContext)
   const [error, setError] = useState('')
   const [loginData, setLoginData] = useState({
     email: '',
     password: '',
   })
+
+  async function submitHandler(){
+    console.log(state)
+    const result = await login(loginData)
+    console.log(result)
+    if (result.status === 200){
+      dispatch({
+        type: AUTH_LOGIN,
+        payload: result.data.username
+      })
+    }else {
+      setError(result.data)
+    }
+    console.log(state.user)
+  }
 
   const emailHandler = (e) => {
     setLoginData({...loginData, email: e.target.value})
@@ -21,12 +39,14 @@ export default function LoginForm() {
   return (
     <>
       <h1>Login</h1>
-      <AuthLabel for="email">Email</AuthLabel>
-      <AuthInput id="email" value={loginData.email}/>
-      <AuthLabel for="password">Password</AuthLabel>
-      <AuthInput type="password" id="password" value={loginData.password}/>
+      <AuthLabel htmlFor="email">Email</AuthLabel>
+      <AuthInput id="email" onChange={emailHandler} value={loginData.email}/>
+      <AuthLabel htmlFor="password">Password</AuthLabel>
+      <AuthInput type="password" id="password" 
+        onChange={passwordHandler} 
+        value={loginData.password}/>
       <ErrorMsg>{error}</ErrorMsg>
-      <AuthBttn>Login</AuthBttn>
+      <AuthBttn onClick={submitHandler}>Login</AuthBttn>
     </>
   )
 }
