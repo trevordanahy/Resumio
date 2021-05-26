176e32db756afc3cdcb375cec68be6d70fb5c9b6
trevordanahy <danat19@protonmail.com>
2021-05-26 13:20:16 -0500

    Added AuthContext to Userbox. Userbox rerenders on login.

--- frontend/src/components/control/UserBox.jsx
+++ frontend/src/components/control/UserBox.jsx
@@ -1,9 +1,20 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
+import AuthContext from '../../context/auth-context'
 import { DisplayedUser, UserBoxContainer, UserBttn } from '../style/UserBoxStyles'
 import AuthModal from './Auth/AuthModal'
 
 export default function UserBox() {
   const [isOpen, setIsOpen] = useState(false)
+  const {state} = useContext(AuthContext)
+
+  if (state.isAuthenticated){
+    return (
+      <UserBoxContainer>
+        <DisplayedUser>{state.user}</DisplayedUser>
+        <UserBttn>Logout</UserBttn>
+      </UserBoxContainer>
+    )
+  }
   
   return (
     <>
